4e9a76443c12a2c15235cfcc4011a0bfa5f9a5f2
Daniel Capurro <dcapurro@gmail.com>
2025-10-15 21:00:16 +1100

    Fix deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/academic-website',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
